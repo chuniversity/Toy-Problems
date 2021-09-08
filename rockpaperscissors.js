43fc42337a27c934c58bec2fa73758e325688bd3
Chuniversity <contact@chunis.org>
2021-09-08 17:03:21 -0400

    rockpaperscissors.js

---
+++ rockpaperscissors.js
@@ -0,0 +1,15 @@
+function rockPaperScissors(rounds) {
+  let result = [];
+  function recurse(substr, depth) {
+    if(depth >= rounds) {
+      return result.push(substr);
+    }
+    recurse(substr + 'r', depth + 1);
+    recurse(substr + 'p', depth + 1);
+    recurse(substr + 's', depth + 1);
+  }
+  recurse('', 0);
+  return result;
+}
+
+// console.log(rockPaperScissors(4));
